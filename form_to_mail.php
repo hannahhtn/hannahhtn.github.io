f387c04d391d5029142edcb76cb86ec50ed4a08a
unknown <hannahhtn97@gmail.com>
2023-03-29 11:23:29 -0700

    test pw

--- form_to_mail.php
+++ form_to_mail.php
@@ -15,13 +15,13 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = "smtp.gmail.com";                       //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'hannahhtn97@gmail.com';                //SMTP username
     $mail->Password   = $env["SCRIPT_PW"];  
-    echo "{$mail->Password}";                   //SMTP password
+    // echo "{$mail->Password}";                   //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
