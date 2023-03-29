241fd95c9557e100bfc8eb4b4b5ca18e9c4f7f60
unknown <hannahhtn97@gmail.com>
2023-03-29 11:58:14 -0700

    fixed authentication bugs

--- form_to_mail.php
+++ form_to_mail.php
@@ -15,14 +15,13 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = "smtp.gmail.com";                       //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'hannahhtn97@gmail.com';                //SMTP username
-    $mail->Password   = getenv('SCRIPT_PW');
-    // echo "{$mail->Password}";                   //SMTP password
-    $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
+    $mail->Password   = getenv('SCRIPT_PW');                    //SMTP password
+    $mail->SMTPSecure = 'ssl';                                  //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     $name = $_POST['Name'];
